996a01255252109a45673b4b3323ce5a58261224
Simon McIntosh-Smith <simonm@cs.bris.ac.uk>
2015-09-30 13:10:11 +0100

    Added comment about variable name conventions
    Explaining the hitherto not seen “tid” and “nthreads”

--- openmp/example1/hello_c.c
+++ openmp/example1/hello_c.c
@@ -5,6 +5,8 @@
 int main (int argc, char *argv[]) {
   
   int nthreads, tid;
+  /* These are commonly used variable names - nthreads is short for Number of threads, while */
+  /* tid is short for Thread ID. These are just conventions though, so you can use your own too */
   
   /* Fork a team of threads giving them their own copies of variables */
 #pragma omp parallel private(tid)
